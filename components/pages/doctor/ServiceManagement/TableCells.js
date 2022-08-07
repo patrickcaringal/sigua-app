1861a505c46707f72ecaf2b19194b7530b85865d
patrickcaringal <patrickcaringalreal@gmail.com>
2022-08-07 20:40:26 +0800

    chore: refactor service shared table cells

--- components/pages/doctor/ServiceManagement/TableCells.js
+++ components/pages/doctor/ServiceManagement/TableCells.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Box, Chip, TableCell, Typography } from "@mui/material";
+import { TableCell, Typography } from "@mui/material";
 
 const TableCells = ({ data }) => {
   const { id, name, description } = data;
@@ -22,36 +22,6 @@ const TableCells = ({ data }) => {
           {description}
         </Typography>
       </TableCell>
-      {/* <TableCell>{name}</TableCell>
-      <TableCell>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            flexWrap: "wrap",
-            gap: 1,
-          }}
-        >
-          {services.map((s, index) => (
-            <Chip key={index} label={s} color="primary" size="small" />
-          ))}
-        </Box>
-      </TableCell>
-      <TableCell>
-        <Typography
-          variant="caption"
-          sx={{
-            display: "-webkit-box",
-            WebkitBoxOrient: "vertical",
-            WebkitLineClamp: "2",
-            overflow: "hidden",
-          }}
-          component="div"
-        >
-          {address}
-        </Typography>
-      </TableCell>
-      <TableCell align="center">{capacity}</TableCell> */}
     </>
   );
 };
